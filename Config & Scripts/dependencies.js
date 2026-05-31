2d745f6b2f5d509fb57ce23be56f2dc675f5d2ba
ziad9053-bit <ziad9053@gmail.com>
2026-06-01 01:55:07 +0300

    Update dependencies.js

--- Config & Scripts/dependencies.js
+++ Config & Scripts/dependencies.js
@@ -1,1 +1,6 @@
+// هنا تضع مفاتيحك الخاصة فقط
+const supabaseUrl = 'https://xywrgfnktvesnmeeqlux.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5d3JnZm5rdHZlc25tZWVxbHV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNzgyMTgsImV4cCI6MjA5NTY1NDIxOH0.mMWZsGlwDimcGoKA96F9nLuXJBE0k3UC9_JYbvqLisI';
 
+// هنا يتم إنشاء الاتصال ليكون متاحاً في كل الموقع
+window.supabase = supabase.createClient(supabaseUrl, supabaseKey);

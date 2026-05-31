f32598842b94afc41103f427f73bdbb33d441d08
ziad9053-bit <ziad9053@gmail.com>
2026-06-01 02:48:42 +0300

    Update js_auth_config.js

--- Config & Scripts/js_auth_config.js
+++ Config & Scripts/js_auth_config.js
@@ -1,1 +1,21 @@
+/**
+ * ملف: js_auth_config.js
+ * المسؤول عن تخزين كلمات المرور وتحديد الصلاحيات
+ */
+const AUTH_CONFIG = {
+    ADMIN_PASS: "123456", // 1111
+    WORKER_PASS: "0000",  // 2424
+};
 
+// دالة التحقق من الدخول
+function verifyLogin(password, type) {
+    if (type === 'admin' && password === AUTH_CONFIG.ADMIN_PASS) {
+        localStorage.setItem('role', 'admin');
+        return true;
+    }
+    if (type === 'worker' && password === AUTH_CONFIG.WORKER_PASS) {
+        localStorage.setItem('role', 'worker');
+        return true;
+    }
+    return false;
+}

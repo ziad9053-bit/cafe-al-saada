138cd66482d0fba927b3d4d55fc81d26ad301316
ziad9053-bit <ziad9053@gmail.com>
2026-06-01 02:06:42 +0300

    Update js_supabase.js

--- Config & Scripts/js_supabase.js
+++ Config & Scripts/js_supabase.js
@@ -1,5 +1,6 @@
-const supabaseUrl = 'https://xywrgfnktvesnmeeqlux.supabase.co'; // أضفت الرابط الكامل
+// هنا تضع مفاتيحك الخاصة فقط
+const supabaseUrl = 'https://xywrgfnktvesnmeeqlux.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5d3JnZm5rdHZlc25tZWVxbHV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNzgyMTgsImV4cCI6MjA5NTY1NDIxOH0.mMWZsGlwDimcGoKA96F9nLuXJBE0k3UC9_JYbvqLisI';
 
-// تعريف العميل وجعله متاحاً عالمياً
+// هنا يتم إنشاء الاتصال ليكون متاحاً في كل الموقع
 window.supabase = supabase.createClient(supabaseUrl, supabaseKey);

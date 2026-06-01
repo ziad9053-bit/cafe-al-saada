5811d5970556a0e61008e3be8c644d842ff0bad6
ziad9053-bit <ziad9053@gmail.com>
2026-06-01 03:25:11 +0300

    Update dependencies.js

--- Config & Scripts/dependencies.js
+++ Config & Scripts/dependencies.js
@@ -2,3 +2,5 @@
 document.write('<script src="https://cdn.tailwindcss.com"></script>');
 document.write('<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>');
 document.write('<script src="js_supabase.js"></script>');
+// تأكد من وجود هذا السطر
+window.supabase = supabase.createClient('YOUR_URL', 'YOUR_KEY');

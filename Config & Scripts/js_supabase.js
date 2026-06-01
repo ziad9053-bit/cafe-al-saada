3d3d1129ea3ad004dad3b9b775d60f44fbc11be3
ziad9053-bit <ziad9053@gmail.com>
2026-06-01 03:50:52 +0300

    Update js_supabase.js

--- Config & Scripts/js_supabase.js
+++ Config & Scripts/js_supabase.js
@@ -2,5 +2,5 @@
 const supabaseUrl = 'https://xywrgfnktvesnmeeqlux.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5d3JnZm5rdHZlc25tZWVxbHV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNzgyMTgsImV4cCI6MjA5NTY1NDIxOH0.mMWZsGlwDimcGoKA96F9nLuXJBE0k3UC9_JYbvqLisI';
 
-// هنا يتم إنشاء الاتصال ليكون متاحاً في كل الموقع
-window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
+/// هنا يتم إنشاء الاتصال ليكون متاحاً في كل الموقع
+window.supabase = supabase.createClient(https://xywrgfnktvesnmeeqlux.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5d3JnZm5rdHZlc25tZWVxbHV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNzgyMTgsImV4cCI6MjA5NTY1NDIxOH0.mMWZsGlwDimcGoKA96F9nLuXJBE0k3UC9_JYbvqLisI);
